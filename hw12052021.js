42d922f27da945ab993b1080e91a6fa6fee4bd69
rgb-sarma <mvacic9@gmail.com>
2021-05-13 11:27:47 +0200

    Task 2 and 3

--- hw12052021.js
+++ hw12052021.js
@@ -1,5 +1,6 @@
 <script>
-    function test(num1, num2) {
+        // task1
+        function test(num1, num2) {
         let temp1 = num1
         let temp2 = num2 
         let counter1 = 0
@@ -38,4 +39,62 @@
     }
 
     console.log(res)
+
+    // task 2
+    function getOddNums(arr) {
+
+        let max = 0
+        let res
+        for (let index in mat) {
+            let counter = 0
+            for(let elm of mat[index]){
+                if (elm < 10) {
+                    counter++
+                }
+            }
+            if (max < counter) {
+                max = counter
+                res = mat[index]
+            }
+        }
+        
+        return res        
+    }
+
+    mat = [[1, 11, 111], [5, 4, 4444], [3, 33, 123]]
+
+    console.log(getOddNums(mat))
+
+    //task 3
+    function reverseNums(str){
+        let res = ''
+        let temp = ''
+        for (let i = 0; i < str.length; i++) {
+            if(!isNaN(parseInt(str.charAt(i)))){
+                temp += str.charAt(i)
+            } else {
+                if (temp !== '') {
+                    for (let i = temp.length - 1; i >= 0; i--) {
+                        res += temp[i]                                              
+                    }
+                }
+                temp = ''
+                res += str.charAt(i)
+            }
+            if (i === str.length - 1) {
+                if (temp !== '') {
+                    for (let i = temp.length - 1; i >= 0; i--) {
+                        res += temp[i]                                              
+                    }
+                }
+            }
+            
+        }
+
+        return res
+    }
+    
+    word = "Danas123je456lep78dan9"
+    
+    console.log(reverseNums(word))
 </script>
